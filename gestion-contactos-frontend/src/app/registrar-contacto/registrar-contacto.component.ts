4bc61be1f18dac361fd50ea275ad078f2a87a5bc
AliciaLilianaEEP <145449008+AliciaLilianaEEP@users.noreply.github.com>
2024-05-13 15:43:17 +0200

    Hago llamada guardarContacto()
    Ya que tengo la llamada al método, hago que cuando regitren un Contacto, se agregue a la base de Datos mySQL y aparezca en la página

--- gestion-contactos-frontend/src/app/registrar-contacto/registrar-contacto.component.ts
+++ gestion-contactos-frontend/src/app/registrar-contacto/registrar-contacto.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Contacto } from '../contacto';
+import { ContactoService } from '../contacto.service';
+import { Router } from '@angular/router';
+import { error } from 'console';
 
 @Component({
   selector: 'app-registrar-contacto',
@@ -8,13 +11,24 @@ import { Contacto } from '../contacto';
 })
 export class RegistrarContactoComponent implements OnInit {
   contacto : Contacto = new Contacto();
-  constructor() { }
+  constructor(private contactoServicio:ContactoService, private router:Router) { }
 
   ngOnInit(): void {
   }
 
+  guardarContacto(){
+    this.contactoServicio.registrarContacto(this.contacto).subscribe(dato => {
+      console.log(dato);
+      this.irAListaDeContactos();
+    }, error => console.log(this.contacto));
+  }
+
+  irAListaDeContactos(){
+    this.router.navigate(['/contactos']);
+  }
+
   onSubmit(){
-    console.log(this.contacto);
+    this.guardarContacto();
   }
 
 }
