544ead5f04f5e84faf7b7132d7e503d945cf0c9d
AliciaLilianaEEP <145449008+AliciaLilianaEEP@users.noreply.github.com>
2024-05-13 20:18:09 +0200

    Actualizo los paths
    Actualizo los pats para llamar a "ActualizarContactoComponent" y ya me funciona el botón de "editar" pero no el de "eliminar"

--- gestion-contactos-frontend/src/app/app-routing.module.ts
+++ gestion-contactos-frontend/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ListaContactosComponent } from './lista-contactos/lista-contactos.component';
 import { RegistrarContactoComponent } from './registrar-contacto/registrar-contacto.component';
+import { ActualizarContactoComponent } from './actualizar-contacto/actualizar-contacto.component';
 
 const routes: Routes = [
   {path : 'contactos', component:ListaContactosComponent},
   {path : '', redirectTo: 'contactos', pathMatch:'full'},
-  {path : 'registrar-contacto', component: RegistrarContactoComponent}
+  {path : 'registrar-contacto', component: RegistrarContactoComponent},
+  {path : 'actualizar-contacto/:id', component : ActualizarContactoComponent}
 ];
 
 @NgModule({
