a70c234eed7bff78592f80289b641d2fae808ca1
AliciaLilianaEEP <145449008+AliciaLilianaEEP@users.noreply.github.com>
2024-05-13 14:46:08 +0200

    Agrego método guardarContacto()
    Agrego un método que me va a servir para guardar al contacto

--- gestion-contactos-backend/src/main/java/com/gestion/contactos/controlador/ContactoControlador.java
+++ gestion-contactos-backend/src/main/java/com/gestion/contactos/controlador/ContactoControlador.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,9 +21,17 @@ public class ContactoControlador {
 	@Autowired
 	private ContactoRepositorio repositorio;
 
+	//Este método sirve para listar todos los contactos
 	@GetMapping("/contactos")
-	public List<Contacto> listarTodosLosEmpleados() {
+	public List<Contacto> listarTodosLosContactos() {
 		return repositorio.findAll();
+	}
+	
+	//Este método me sirve para guardar el contacto
+	@PostMapping("/contactos")
+	public Contacto guardarContacto(@RequestBody Contacto contacto) {
+		return repositorio.save(contacto);
+		
 	}
 
 }
