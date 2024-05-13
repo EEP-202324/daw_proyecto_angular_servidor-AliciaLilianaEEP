e7edc0936cb56ed8a5c055347cd2b5e29bbab4c3
AliciaLilianaEEP <145449008+AliciaLilianaEEP@users.noreply.github.com>
2024-05-13 14:40:43 +0200

    Cambio títulos, actualizo rutas, y agrego Header
    Cambio los títulos de "gestion-contactos-frontend" a  "Sistema gestión de contactos" para que sean más legibles y configuro las rutas para cuando entre en un navegador

--- gestion-contactos-frontend/src/app/app-routing.module.ts
+++ gestion-contactos-frontend/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListaContactosComponent } from './lista-contactos/lista-contactos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : 'contactos', component:ListaContactosComponent},
+  {path : '', redirectTo: 'contactos', pathMatch:'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
