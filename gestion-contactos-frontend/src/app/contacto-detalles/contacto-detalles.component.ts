c9bf2eadc393e207799b4b8cd476d7e9f00d65a7
AliciaLilianaEEP <145449008+AliciaLilianaEEP@users.noreply.github.com>
2024-05-18 21:20:50 +0200

    Creo componente contacto-detalles
    Creo componente para que pueda ser mi apartado de detalles de cada contacto

---
+++ gestion-contactos-frontend/src/app/contacto-detalles/contacto-detalles.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contacto-detalles',
+  templateUrl: './contacto-detalles.component.html',
+  styleUrl: './contacto-detalles.component.css'
+})
+export class ContactoDetallesComponent {
+
+}
