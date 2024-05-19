7a1c727743f12649861915475cccf6f59fc42e55
AliciaLilianaEEP <145449008+AliciaLilianaEEP@users.noreply.github.com>
2024-05-19 02:09:24 +0200

    Actualizar-contacto.component Funciona
    Hago las modificaciones en actualizar-contacto.component.ts && en actualizar-contacto.component.html para que funcione el botón de editar.//// Instalo sweetalert2 para que se me muestren alertas más  atractivas

--- gestion-contactos-frontend/src/app/actualizar-contacto/actualizar-contacto.component.ts
+++ gestion-contactos-frontend/src/app/actualizar-contacto/actualizar-contacto.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Contacto } from '../contacto';
+import { ContactoService } from '../contacto.service';
+import { Router, ActivatedRoute } from '@angular/router';
+import swal from 'sweetalert2';
 
 @Component({
   selector: 'app-actualizar-contacto',
   templateUrl: './actualizar-contacto.component.html',
-  styleUrl: './actualizar-contacto.component.css'
+  styleUrls: ['./actualizar-contacto.component.css']
 })
-export class ActualizarContactoComponent {
+export class ActualizarContactoComponent implements OnInit{
 
+  id:number;
+  contacto:Contacto = new Contacto();
+  constructor(private contactoService:ContactoService, private router:Router, private route:ActivatedRoute){}
+
+  ngOnInit(): void {
+    this.id = this.route.snapshot.params['id'];
+    this.contactoService.obtenerContactoPorId(this.id).subscribe(dato =>{
+      this.contacto = dato;
+    }, error => console.log(error));
+  }
+
+  irAlaListadeContactos(){
+    this.router.navigate(['/contactos']);
+    swal.fire('Contacto actualizado', `El contacto ${this.contacto.nombre} ha sido actualizado con exito`,`success`);
+  }
+
+  onSubmit(){
+    this.contactoService.actualizarContacto(this.id, this.contacto).subscribe(dato =>{
+      this.irAlaListadeContactos();
+    }, error => console.log(error));
+  }
 }
